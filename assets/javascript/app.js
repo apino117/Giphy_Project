6e495eba40d26c5cca09f08e4bf221900f98a625
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-03-14 17:31:42 -0400

    assembled skeleton and made the ajax url

---
+++ assets/javascript/app.js
@@ -0,0 +1,2 @@
+// This is the guery url for giphy with the API key. In this example it's for "cat" with a limit of one and a rating of g.
+// https://api.giphy.com/v1/gifs/search?q=cat&limit=1&rating=g&api_key=Q6wuNS9yXqx9RCJVlHTGwHvkcJXhobE8
